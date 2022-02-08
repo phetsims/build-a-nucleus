abb313ad444a9e2a469b66db60341b81b0cbd505
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-02-08 13:49:33 -0700

    Remove unnecessary tandem since tandem is created in the option types. See https://github.com/phetsims/build-a-nucleus/issues/8.

--- js/build-a-nucleus-main.ts
+++ js/build-a-nucleus-main.ts
@@ -8,7 +8,6 @@
 
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
-import Tandem from '../../tandem/js/Tandem.js';
 import buildANucleusStrings from './buildANucleusStrings.js';
 import DecayScreen from './decay/DecayScreen.js';
 
@@ -32,7 +31,7 @@ const simOptions = {
 // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
   const sim = new Sim( buildANucleusTitleString, [
-    new DecayScreen( Tandem.ROOT.createTandem( 'buildANucleusScreen' ) )
+    new DecayScreen()
   ], simOptions );
   sim.start();
 } );
