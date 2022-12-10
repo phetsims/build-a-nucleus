de1c7177b27d8810296b732f127e3b133a337fb2
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-12-09 18:27:42 -0700

    Create ParticleNucleus.ts, the model for the nucleon energy levels. See https://github.com/phetsims/build-a-nucleus/issues/46.

---
+++ js/chart-intro/model/ParticleNucleus.ts
@@ -0,0 +1,87 @@
+// Copyright 2022, University of Colorado Boulder
+
+import { ObservableArray } from '../../../../axon/js/createObservableArray.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import Particle from '../../../../shred/js/model/Particle.js';
+import ParticleAtom from '../../../../shred/js/model/ParticleAtom.js';
+import buildANucleus from '../../buildANucleus.js';
+import BANConstants from '../../common/BANConstants.js';
+
+/**
+ * A model element that represents a nucleus that is made up of protons and neutrons. This model element
+ * manages the positions and motion of all particles that are a part of the nucleus.
+ *
+ * @author Marla Schulz (PhET Interactive Simulations)
+ * @author Luisa Vargas
+ */
+
+type ParticleShellPosition = {
+  particle: Particle | null;
+  xPosition: number; // 0 - 5
+};
+
+// constants
+const ALLOWED_PARTICLE_POSITIONS = [
+  [ 2, 3 ],
+  [ 0, 1, 2, 3, 4, 5 ],
+  [ 0, 1, 2, 3, 4, 5 ]
+];
+
+class ParticleNucleus extends ParticleAtom {
+
+  public readonly protonShellPositions: ParticleShellPosition[][];
+  public readonly neutronShellPositions: ParticleShellPosition[][];
+  public modelViewTransform: ModelViewTransform2;
+
+  public constructor() {
+    super();
+
+    const particleWidth = BANConstants.PARTICLE_RADIUS * 2;
+    this.modelViewTransform = ModelViewTransform2.createRectangleInvertedYMapping( new Bounds2( 0, 0, 5, 2 ),
+      new Bounds2( 0, 0, particleWidth * 6, particleWidth * 8 ) );
+
+    // Initialize the positions where a nucleon can be placed.
+    this.protonShellPositions = [
+      [], [], []
+    ];
+    this.neutronShellPositions = [
+      [], [], []
+    ];
+    for ( let i = 0; i < ALLOWED_PARTICLE_POSITIONS.length; i++ ) {
+      for ( let j = 0; j < ALLOWED_PARTICLE_POSITIONS[ i ].length; j++ ) {
+        const shellPosition = { particle: null, xPosition: ALLOWED_PARTICLE_POSITIONS[ i ][ j ] };
+        this.protonShellPositions[ i ][ ALLOWED_PARTICLE_POSITIONS[ i ][ j ] ] = shellPosition;
+        this.neutronShellPositions[ i ][ ALLOWED_PARTICLE_POSITIONS[ i ][ j ] ] = shellPosition;
+      }
+    }
+  }
+
+  public override reconfigureNucleus(): void {
+
+    // fill all nucleons in open positions from bottom to top, left to right
+    const updateNucleonPositions = ( particleArray: ObservableArray<Particle>, oldNucleonCount: number, particleShellPositions: ParticleShellPosition[][] ) => {
+      const currentNucleonCount = particleArray.length;
+      let nucleonIndex = 0;
+      if ( currentNucleonCount !== oldNucleonCount ) {
+        particleShellPositions.forEach( nucleonShellPositions => {
+          nucleonShellPositions.forEach( nucleonShellPosition => {
+            if ( nucleonIndex < currentNucleonCount ) {
+              nucleonShellPosition.particle = particleArray[ nucleonIndex ];
+              nucleonIndex++;
+            }
+            else {
+              nucleonShellPosition.particle = null;
+            }
+          } );
+        } );
+      }
+    };
+
+    updateNucleonPositions( this.protons, 0, this.protonShellPositions );
+    updateNucleonPositions( this.neutrons, 0, this.neutronShellPositions );
+  }
+}
+
+buildANucleus.register( 'ParticleNucleus', ParticleNucleus );
+export default ParticleNucleus;
