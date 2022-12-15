c041c293ba3939e54e454bf0dfe3a3dae6e47041
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-12-14 20:19:36 -0700

    Add 'highlight' rectangle that highlights the current neutron number. Add documentation to NucleonNumberLine.ts. See https://github.com/phetsims/build-a-nucleus/issues/46.

--- js/chart-intro/view/NucleonNumberLine.ts
+++ js/chart-intro/view/NucleonNumberLine.ts
@@ -6,7 +6,7 @@
  * @author Luisa Vargas
  */
 
-import { Color, Node, Text } from '../../../../scenery/js/imports.js';
+import { Color, Node, Rectangle, Text } from '../../../../scenery/js/imports.js';
 import buildANucleus from '../../buildANucleus.js';
 import ParticleType from '../../common/view/ParticleType.js';
 import BuildANucleusStrings from '../../BuildANucleusStrings.js';
@@ -21,6 +21,7 @@ import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransfo
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import ArrowNode from '../../../../scenery-phet/js/ArrowNode.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import BANColors from '../../common/BANColors.js';
 
 class NucleonNumberLine extends Node {
 
@@ -30,12 +31,6 @@ class NucleonNumberLine extends Node {
     assert && assert( particleType === ParticleType.PROTON || particleType === ParticleType.NEUTRON,
       'The particleType should be of type PROTON or NEUTRON. particleType = ' + particleType.name );
 
-    // const numberHighlightRectangle = new Rectangle( {
-    //   rectSize: new Dimension2( 8, 12 ), // TODO: base this height and width off of the legend text size
-    //   fill: particleType === ParticleType.PROTON ? BANColors.protonColorProperty : BANColors.neutronColorProperty
-    // } );
-    // this.addChild( numberHighlightRectangle );
-
     const modelXStartRange = particleType === ParticleType.PROTON ?
                              BANConstants.DEFAULT_INITIAL_PROTON_COUNT : BANConstants.DEFAULT_INITIAL_NEUTRON_COUNT;
     const modelXEndRange = particleType === ParticleType.PROTON ? 10 : 12; // TODO: magic numbers or fine?
@@ -43,7 +38,6 @@ class NucleonNumberLine extends Node {
     const numberLineLength = new Range( modelXStartRange, modelXEndRange ).getLength();
     const tickMarkLength = viewWidth / numberLineLength;
 
-    // TODO: Fix the view Y's
     let modelViewTransform: ModelViewTransform2;
     if ( orientation === Orientation.HORIZONTAL ) {
       modelViewTransform = ModelViewTransform2.createRectangleMapping(
@@ -57,7 +51,6 @@ class NucleonNumberLine extends Node {
         new Bounds2( 0, 0, tickMarkLength, viewWidth )
       );
     }
-
 
     const numberLineNode = new Node();
     let chartTransform;
@@ -75,7 +68,7 @@ class NucleonNumberLine extends Node {
       } );
     }
 
-
+    // create and add the tick marks
     const tickXSpacing = 1;
     const tickMarkSet = new TickMarkSet( chartTransform, orientation, tickXSpacing, {
       stroke: Color.BLACK,
@@ -83,6 +76,7 @@ class NucleonNumberLine extends Node {
     } );
     numberLineNode.addChild( tickMarkSet );
 
+    // create and add the tick labels
     const tickLabelSet = new TickLabelSet( chartTransform, orientation, tickXSpacing, {
       extent: 5,
       createLabel: ( value: number ) => new Text( value, {
@@ -96,6 +90,8 @@ class NucleonNumberLine extends Node {
           label.centerTop = tickBounds.centerBottom.plusXY( modelViewTransform.modelToViewX( -0.5 ), 1 );
         }
         else {
+
+          // ticks flow vertically, so tick labels should be to the left
           label.rightCenter = tickBounds.leftCenter.plusXY( -1, modelViewTransform.modelToViewY( 0.5 ) );
         }
         return label;
@@ -103,6 +99,36 @@ class NucleonNumberLine extends Node {
     } );
     numberLineNode.addChild( tickLabelSet );
 
+    // create and add the rectangle that would highlight the current particle count number on the number line
+    const numberHighlightRectangle = new Rectangle( {
+      // TODO: base this height and width off of the legend text size
+      rectWidth: 8,
+      rectHeight: 12,
+      fill: particleType === ParticleType.PROTON ? BANColors.protonColorProperty : BANColors.neutronColorProperty
+    } );
+    if ( orientation === Orientation.HORIZONTAL ) {
+      numberHighlightRectangle.centerY = tickLabelSet.centerY;
+
+      // 'highlight' the label with the current particleCount on the tickLabelSet
+      particleCountProperty.link( particleCount => {
+        numberHighlightRectangle.x = tickLabelSet.left + ( particleCount * 25 );
+
+        // for double digits double the width and adjust where the highlight starts
+        if ( particleCount > 9 ) {
+          numberHighlightRectangle.rectWidth = 16;
+          numberHighlightRectangle.x -= 4;
+        }
+        else {
+          numberHighlightRectangle.rectWidth = 8;
+        }
+      } );
+    }
+    else {
+      numberHighlightRectangle.centerX = tickLabelSet.centerX;
+    }
+    this.addChild( numberHighlightRectangle );
+
+    // create and add the arrow to the number line
     const tailX = particleType === ParticleType.NEUTRON ? modelViewTransform.modelToViewX( modelXStartRange - 1 ) : tickMarkSet.centerX;
     const tailY = particleType === ParticleType.NEUTRON ? tickMarkSet.centerY : modelViewTransform.modelToViewY( modelXStartRange + 1 );
     const tipX = particleType === ParticleType.NEUTRON ? modelViewTransform.modelToViewX( modelXEndRange + 1 ) : tickMarkSet.centerX;
@@ -115,6 +141,7 @@ class NucleonNumberLine extends Node {
     numberLineNode.addChild( numberLine );
     this.addChild( numberLineNode );
 
+    // create and add the number line axis label
     const numberLineLabel = new Text( StringUtils.fillIn( BuildANucleusStrings.nucleonNumber, {
       nucleonType: particleType.label
     } ), { fontSize: 14 } );
