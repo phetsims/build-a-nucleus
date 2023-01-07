c70c87982b447489e32312033162326299b7ab8c
phet-dev <phetadmin@gmail.com>
2023-01-07 03:59:23 -0700

    update copyright dates from daily grunt work

--- js/chart-intro/model/ParticleNucleus.ts
+++ js/chart-intro/model/ParticleNucleus.ts
@@ -1,4 +1,4 @@
-// Copyright 2022, University of Colorado Boulder
+// Copyright 2022-2023, University of Colorado Boulder
 
 import { ObservableArray } from '../../../../axon/js/createObservableArray.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
