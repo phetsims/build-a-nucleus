66b447695cf2f9a80f30acddaa91ccfdd90a4346
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2023-03-04 20:03:47 -0700

    Highkight cell in NuclideChartNode based on nuclide built. See https://github.com/phetsims/build-a-nucleus/issues/46.

--- js/chart-intro/view/NuclideChartCell.ts
+++ js/chart-intro/view/NuclideChartCell.ts
@@ -6,13 +6,19 @@
  * @author Luisa Vargas
  */
 
-import { Color, Rectangle } from '../../../../scenery/js/imports.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import { Text, Color, Rectangle } from '../../../../scenery/js/imports.js';
 import buildANucleus from '../../buildANucleus.js';
+import DecayType from '../../common/view/DecayType.js';
 
+// constants
 const CELL_LENGTH = 25;
+
 class NuclideChartCell extends Rectangle {
 
-  public constructor( normalFill: Color ) {
+  private readonly labelText: Text;
+
+  public constructor( normalFill: Color, elementSymbol: string ) {
 
     super( 0, 0, CELL_LENGTH, CELL_LENGTH, 0, 0, {
       stroke: 'black',
@@ -20,6 +26,23 @@ class NuclideChartCell extends Rectangle {
       fill: normalFill
     } );
 
+    // labels the cell with the elementSymbol
+    this.labelText = new Text( elementSymbol, {
+      font: new PhetFont( 14 ),
+      center: this.center,
+      fill: normalFill === DecayType.ALPHA_DECAY.colorProperty.value ||
+            normalFill === DecayType.BETA_MINUS_DECAY.colorProperty.value ?
+            Color.BLACK : Color.WHITE,
+      maxWidth: CELL_LENGTH - 5
+    } );
+    this.labelText.visible = false;
+    this.addChild( this.labelText );
+
+  }
+
+  // show the label text when highlighting the cell
+  public setHighlighted( highlighted: boolean ): void {
+    this.labelText.visible = highlighted;
   }
 }
 
--- js/chart-intro/view/NuclideChartNode.ts
+++ js/chart-intro/view/NuclideChartNode.ts
@@ -14,25 +14,13 @@ import NuclideChartCell from './NuclideChartCell.js';
 import BANColors from '../../common/BANColors.js';
 import DecayType from '../../common/view/DecayType.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
+import Multilink from '../../../../axon/js/Multilink.js';
 
 // constants
-// 2D array that defines the table structure. The rows are the proton number and the number is the neutron number.
-/*
-const POPULATED_CELLS = [
-  [ 1, 4, 6 ],
-  [ 0, 1, 2, 3, 4, 5, 6 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8 ],
-  [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
-  [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
-  [ 5, 6, 7, 8, 9, 10, 11, 12 ]
-];
-*/
-
+// 2D array that defines the table structure.
+// The rows are the proton number, in reverse. For example, the last row is protonNumber = 0, while the first row is
+// protonNumber = POPULATED_CELLS.length - 1 = 10
+// The numbers in the rows are the neutronNumber.
 const POPULATED_CELLS = [
   [ 5, 6, 7, 8, 9, 10, 11, 12 ],
   [ 4, 5, 6, 7, 8, 9, 10, 11, 12 ],
@@ -49,16 +37,18 @@ const POPULATED_CELLS = [
 
 class NuclideChartNode extends Node {
 
-  private cells: NuclideChartCell[];
+  // the cells of the table
+  private readonly cells: NuclideChartCell[][];
 
   public constructor( protonCountProperty: TReadOnlyProperty<number>, neutronCountProperty: TReadOnlyProperty<number> ) {
     super();
 
     const cellDimension = 25;
-    this.cells = [];
+    this.cells = []; // TODO: do I need to dispose the cells? otherwise doesn't need to be class property
     for ( let p = 0; p < POPULATED_CELLS.length; p++ ) {
       const populatedCellsInRow = POPULATED_CELLS[ p ];
-      const currentProtonLoopCount = POPULATED_CELLS.length - p - 1;
+      const currentProtonLoopCount = POPULATED_CELLS.length - p - 1; // the current proton count in the loop
+      const rowCells = [];
       for ( let n = 0; n < populatedCellsInRow.length; n++ ) {
 
         // get first decay in available decays to color the cell according to that decay type
@@ -67,12 +57,31 @@ class NuclideChartNode extends Node {
         const color = AtomIdentifier.isStable( currentProtonLoopCount, populatedCellsInRow[ n ] ) ? BANColors.stableColorProperty.value :
                       decayType === undefined ? BANColors.unknownColorProperty.value : // no available decays, unknown decay type
                       DecayType.enumeration.getValue( decayType.toString() ).colorProperty.value;
-        const cell = new NuclideChartCell( color );
+
+        const elementSymbol = AtomIdentifier.getSymbol( currentProtonLoopCount );
+        const cell = new NuclideChartCell( color, elementSymbol );
         cell.translation = new Vector2( populatedCellsInRow[ n ] * cellDimension, p * cellDimension );
         this.addChild( cell );
-        this.cells.push( cell );
+        rowCells.push( cell );
       }
+      this.cells.push( rowCells );
     }
+
+    // Highlight the cell that corresponds to the nuclide.
+    let highlightedCell: NuclideChartCell | null = null;
+    Multilink.multilink( [ protonCountProperty, neutronCountProperty ], ( protonCount: number, neutronCount: number ) => {
+      if ( highlightedCell !== null ) {
+        highlightedCell.setHighlighted( false );
+      }
+
+      // highlight the cell if it exists
+      if ( AtomIdentifier.doesExist( protonCount, neutronCount ) && ( protonCount !== 0 || neutronCount !== 0 ) ) {
+        const protonRowIndex = POPULATED_CELLS.length - protonCount - 1;
+        const neutronRowIndex = POPULATED_CELLS[ protonRowIndex ].indexOf( neutronCount );
+        highlightedCell = this.cells[ protonRowIndex ][ neutronRowIndex ];
+        highlightedCell.setHighlighted( true );
+      }
+    } );
   }
 }
 
