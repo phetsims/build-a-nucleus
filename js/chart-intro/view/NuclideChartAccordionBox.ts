02ffb6dc7dee4292448d10c16ae38da40d516ced
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2023-05-09 15:30:39 -0600

    Create and add ZoomInChartNode.ts which clips a highlighted area of the nuclide chart node. https://github.com/phetsims/build-a-nucleus/issues/46.

--- js/chart-intro/view/NuclideChartAccordionBox.ts
+++ js/chart-intro/view/NuclideChartAccordionBox.ts
@@ -1,24 +1,25 @@
 // Copyright 2023, University of Colorado Boulder
 
-import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
-import AccordionBox from '../../../../sun/js/AccordionBox.js';
-import buildANucleus from '../../buildANucleus.js';
-import NuclideChartAndNumberLines from './NuclideChartAndNumberLines.js';
-import BuildANucleusStrings from '../../BuildANucleusStrings.js';
-import { Color, HBox, Rectangle, Text, VBox } from '../../../../scenery/js/imports.js';
-import NuclideChartLegendNode from './NuclideChartLegendNode.js';
-import { SelectedChartType } from '../model/ChartIntroModel.js';
-import BANConstants from '../../common/BANConstants.js';
-import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
-import Range from '../../../../dot/js/Range.js';
-import FocusedNuclideChartNode from './FocusedNuclideChartNode.js';
-
 /**
  * Node that holds Nuclide Chart and Zoom-in nuclide chart view.
  *
  * @author Luisa Vargas
  * @author Marla Schulz (PhET Interactive Simulations)
  */
+
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import AccordionBox from '../../../../sun/js/AccordionBox.js';
+import buildANucleus from '../../buildANucleus.js';
+import NuclideChartAndNumberLines from './NuclideChartAndNumberLines.js';
+import BuildANucleusStrings from '../../BuildANucleusStrings.js';
+import { Color, HBox, Text, VBox } from '../../../../scenery/js/imports.js';
+import NuclideChartLegendNode from './NuclideChartLegendNode.js';
+import { SelectedChartType } from '../model/ChartIntroModel.js';
+import BANConstants from '../../common/BANConstants.js';
+import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
+import Range from '../../../../dot/js/Range.js';
+import FocusedNuclideChartNode from './FocusedNuclideChartNode.js';
+import ZoomInChartNode from './ZoomInChartNode.js';
 
 class NuclideChartAccordionBox extends AccordionBox {
 
@@ -34,29 +35,26 @@ class NuclideChartAccordionBox extends AccordionBox {
 
     const partialChartTransform = getChartTransform( 20 );
     const focusedChartTransform = getChartTransform( 10 );
+    const zoomInChartTransform = getChartTransform( 30 );
 
     const nuclideChartAndNumberLines = new NuclideChartAndNumberLines( protonCountProperty, neutronCountProperty,
       partialChartTransform );
 
     const focusedNuclideChartNode = new FocusedNuclideChartNode( protonCountProperty, neutronCountProperty,
       focusedChartTransform );
+    const zoomInNuclideChartNode = new ZoomInChartNode( protonCountProperty, neutronCountProperty,
+      zoomInChartTransform );
     const nuclideChartLegendNode = new NuclideChartLegendNode();
 
-    const zoomInChart = new Rectangle( 0, 0, 100, 100, {
-      stroke: Color.BLACK,
-      layoutOptions: {
-        topMargin: 3
-      }
-    } );
     selectedNuclideChartProperty.link( selectedNuclideChart => {
-      zoomInChart.visible = selectedNuclideChart === 'zoom';
+      zoomInNuclideChartNode.visible = selectedNuclideChart === 'zoom';
       focusedNuclideChartNode.visible = selectedNuclideChart === 'zoom';
       nuclideChartAndNumberLines.visible = selectedNuclideChart === 'partial';
     } );
 
     const chartsHBox = new HBox( {
       children: [
-        zoomInChart,
+        zoomInNuclideChartNode,
         nuclideChartAndNumberLines,
         focusedNuclideChartNode
       ],
---
+++ js/chart-intro/view/ZoomInChartNode.ts
@@ -0,0 +1,42 @@
+// Copyright 2023, University of Colorado Boulder
+
+/**
+ * Node that shows zoom-in on nuclide chart.
+ *
+ * @author Luisa Vargas
+ * @author Marla Schulz (PhET Interactive Simulations)
+ */
+
+import buildANucleus from '../../buildANucleus.js';
+import NuclideChartNode from './NuclideChartNode.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
+import { Shape } from '../../../../kite/js/imports.js';
+import Multilink from '../../../../axon/js/Multilink.js';
+import AtomIdentifier from '../../../../shred/js/AtomIdentifier.js';
+
+class ZoomInChartNode extends NuclideChartNode {
+
+  public constructor( protonCountProperty: TReadOnlyProperty<number>, neutronCountProperty: TReadOnlyProperty<number>,
+                      chartTransform: ChartTransform ) {
+    super( protonCountProperty, neutronCountProperty, chartTransform, { cellTextFontSize: 18, arrowSymbol: false } );
+
+    const squareLength = chartTransform.modelToViewDeltaX( 5 );
+
+    Multilink.multilink( [ protonCountProperty, neutronCountProperty ], ( protonCount, neutronCount ) => {
+      const cellX = neutronCount;
+      const cellY = protonCount;
+      if ( AtomIdentifier.doesExist( protonCount, neutronCount ) ) {
+
+        // clip chart to 2 cellLength's around current cell
+        const clipArea = Shape.rectangle( chartTransform.modelToViewX( cellX - 2 ), chartTransform.modelToViewY( cellY + 2 ),
+          squareLength, squareLength );
+        this.clipArea = clipArea;
+      }
+    } );
+
+  }
+}
+
+buildANucleus.register( 'ZoomInChartNode', ZoomInChartNode );
+export default ZoomInChartNode;
