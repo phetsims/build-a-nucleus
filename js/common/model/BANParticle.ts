50cb621c3a78993bb40f06137848eb89300b62bc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-08-01 15:24:05 -0600

    convert to typescript, https://github.com/phetsims/shred/issues/38

--- js/common/model/BANParticle.ts
+++ js/common/model/BANParticle.ts
@@ -6,14 +6,14 @@
  * @author Luisa Vargas
  */
 
-import Particle, { ParticleOptions } from '../../../../shred/js/model/Particle.js';
+import Particle, { ParticleOptions, ParticleType } from '../../../../shred/js/model/Particle.js';
 import BANConstants from '../BANConstants.js';
 import buildANucleus from '../../buildANucleus.js';
 
 export type BANParticleOptions = ParticleOptions;
 export default class BANParticle extends Particle {
 
-  public constructor( type: string, providedOptions?: BANParticleOptions ) {
+  public constructor( type: ParticleType, providedOptions?: BANParticleOptions ) {
     super( type, providedOptions );
     this.animationVelocityProperty.value = BANConstants.PARTICLE_ANIMATION_SPEED;
   }
