90d926a1cc7c947d39a6529443dc7f1074ff10fe
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2023-08-08 18:17:37 -0600

    Remove unused variable since FIRST_LEVEL_CAPACITY and SECOND_LEVEL_CAPACITY constants are being used instead. See https://github.com/phetsims/build-a-nucleus/issues/85.

--- js/chart-intro/model/EnergyLevelType.ts
+++ js/chart-intro/model/EnergyLevelType.ts
@@ -12,23 +12,21 @@ import Enumeration from '../../../../phet-core/js/Enumeration.js';
 
 class EnergyLevelType extends EnumerationValue {
 
-  public static readonly NONE = new EnergyLevelType( 0, 0, 0 );
+  public static readonly NONE = new EnergyLevelType( 0, 0 );
 
-  public static readonly FIRST = new EnergyLevelType( 1, 0, 3 );
+  public static readonly FIRST = new EnergyLevelType( 1, 0 );
 
-  public static readonly SECOND = new EnergyLevelType( 2, 0, 9 );
+  public static readonly SECOND = new EnergyLevelType( 2, 0 );
 
   public static readonly enumeration = new Enumeration( EnergyLevelType );
 
   public readonly yPosition: number;
   public readonly xPosition: number;
-  public readonly fullPlusOneNumber: number;
 
-  public constructor( yPosition: number, xPosition: number, fullPlusOneNumber: number ) {
+  public constructor( yPosition: number, xPosition: number ) {
     super();
 
     this.yPosition = yPosition;
-    this.fullPlusOneNumber = fullPlusOneNumber;
     this.xPosition = xPosition;
   }
 }
