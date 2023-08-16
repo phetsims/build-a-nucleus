11166b895a43310e7efb8de42835b4f33df73933
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2023-08-16 16:16:53 -0600

    Remove dead code. See https://github.com/phetsims/build-a-nucleus/issues/42.

--- js/decay/model/DecayModel.ts
+++ js/decay/model/DecayModel.ts
@@ -77,13 +77,6 @@ class DecayModel extends BANModel<ParticleAtom> {
                                          hasIncomingParticles: boolean ): boolean => {
       const decays = AtomIdentifier.getAvailableDecaysAndPercents( protonNumber, neutronNumber );
 
-      // TODO: remove with https://github.com/phetsims/build-a-nucleus/issues/42
-      // Disallow having more protons that Uranium. Though this is not scientifically accurate, it keeps the model in
-      // the confines of 92 protons. See https://github.com/phetsims/build-a-nucleus/issues/42
-      // if ( protonNumber === 92 && neutronNumber === 145 && decayType === DecayType.BETA_MINUS_DECAY ) {
-      //   return false;
-      // }
-
       return !hasIncomingParticles && !!decays.find( decay => Object.keys( decay ).includes( decayType.name ) );
     };
 
