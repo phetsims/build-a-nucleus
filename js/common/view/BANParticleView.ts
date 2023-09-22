b3109ca8867960592b0663ee2b921a2884282a62
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-09-22 12:47:31 -0600

    bring back query parameter, https://github.com/phetsims/build-a-nucleus/issues/197

--- js/common/view/BANParticleView.ts
+++ js/common/view/BANParticleView.ts
@@ -12,6 +12,7 @@ import ParticleView, { ParticleViewOptions } from '../../../../shred/js/view/Par
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import BANConstants from '../BANConstants.js';
+import BANQueryParameters from '../BANQueryParameters.js';
 
 export type BANParticleViewOptions = ParticleViewOptions;
 
@@ -19,7 +20,7 @@ export default class BANParticleView extends ParticleView {
 
   public constructor( particle: Particle, modelViewTransform: ModelViewTransform2, providedOptions?: BANParticleViewOptions ) {
     const options = optionize<BANParticleViewOptions, EmptySelfOptions, ParticleViewOptions>()( {
-      touchOffset: new Vector2( 0, -BANConstants.PARTICLE_RADIUS * 3 ) // empirically determined
+      touchOffset: new Vector2( 0, -BANQueryParameters.particleTouchOffset ) // empirically determined
     }, providedOptions );
     super( particle, modelViewTransform, options );
   }
