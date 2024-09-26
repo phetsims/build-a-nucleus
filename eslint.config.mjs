c2b1ce1eb3b15739c848a336bc74f72285c64426
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:03 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for build-a-nucleus.
+ * ESLint configuration for build-a-nucleus.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
