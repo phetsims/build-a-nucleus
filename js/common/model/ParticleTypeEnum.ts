d88c7f03bbeea571d9ebf6c2edaddb95f1e774d3
phet-dev <phetadmin@gmail.com>
2026-03-20 05:17:13 -0600

    update copyright dates from daily grunt work

--- js/common/model/ParticleTypeEnum.ts
+++ js/common/model/ParticleTypeEnum.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2025, University of Colorado Boulder
+// Copyright 2022-2026, University of Colorado Boulder
 
 /**
  * ParticleTypeEnum is an enumeration type that identifies the particle types and associates these names with other
